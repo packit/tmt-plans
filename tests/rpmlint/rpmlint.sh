edd5753ffd8fe8c7d084be69ce142d9ab2887095
Cristian Le <cristian.le@mpsd.mpg.de>
2024-01-12 17:55:42 +0100

    support rpmlint for EPEL
    Signed-off-by: Cristian Le <cristian.le@mpsd.mpg.de>

--- tests/rpmlint/rpmlint.sh
+++ tests/rpmlint/rpmlint.sh
@@ -7,13 +7,15 @@ rlJournalStart
 		rlRun "tmp=\$(mktemp -d)" 0 "Create tmp directory"
 		rlRun "pushd $tmp"
 		rlRun "set -o pipefail"
+		rlIsCentOS && rlLogWarning "rpmlint on CentOS+Epel is outdated. Reported information might not be relevant"
 	rlPhaseEnd
 
 	rlPhaseStartTest
 		rlRun "rpm2cpio /var/share/test-artifacts/*.src.rpm | cpio -civ '*.spec'" 0 "Extract spec file from srpm"
 		rlRun "cp ./*.spec $TMT_TEST_DATA/" 0 "Expose spec file"
 		# The spec rpmlint is already executed from the srpm
-		rlRun "rpmlint -c $TMT_PLAN_DATA/packit.toml /var/share/test-artifacts/*.rpm" 0 "Run rpmlint"
+		rlIsCentOS || rlRun "rpmlint -c $TMT_PLAN_DATA/packit.toml /var/share/test-artifacts/*.rpm" 0 "Run rpmlint (non-CentOS)"
+		rlIsCentOS && rlRun "rpmlint /var/share/test-artifacts/*.rpm || true" 0 "Run rpmlint (CentOS)"
 	rlPhaseEnd
 
 	rlPhaseStartCleanup
