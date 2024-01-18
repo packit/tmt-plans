7831c84561fb87d60143e0c017738ba10005c82d
Cristian Le <cristian.le@mpsd.mpg.de>
2024-01-18 17:38:41 +0100

    Report rpmlint results
    Signed-off-by: Cristian Le <cristian.le@mpsd.mpg.de>

--- tests/rpmlint/rpmlint.sh
+++ tests/rpmlint/rpmlint.sh
@@ -29,8 +29,19 @@ rlJournalStart
 			[[ -f "$TMT_PLAN_DATA/${BASH_REMATCH[1]}.rpmlintrc" ]] && rlRun "rc_file=$TMT_PLAN_DATA/${BASH_REMATCH[1]}.rpmlintrc" 0 "Use the rpmlintrc file of $spec_file"
 		done
 		# The spec rpmlint is already executed from the srpm
-		rlIsCentOS || rlRun "rpmlint -c $config_file ${rc_file:+"-r $rc_file"} /var/share/test-artifacts/*.rpm" 0 "Run rpmlint (non-CentOS)"
-		rlIsCentOS && rlRun "rpmlint /var/share/test-artifacts/*.rpm || true" 0 "Run rpmlint (CentOS)"
+		rlIsCentOS || rlRun -s "rpmlint -c $config_file ${rc_file:+"-r $rc_file"} /var/share/test-artifacts/*.rpm" 0 "Run rpmlint (non-CentOS)"
+		rlIsCentOS && rlRun -s "rpmlint /var/share/test-artifacts/*.rpm || true" 0 "Run rpmlint (CentOS)"
+		# Read the output and extract the error/warning messages
+		re_pattern="([0-9]+) packages and ([0-9]+) specfiles checked; ([0-9]+) errors, ([0-9]+) warnings(, ([0-9]+) filtered, ([0-9]+) badness)?"
+		while IFS= read -r line; do
+			if [[ $line =~ $re_pattern ]]; then
+				rlRun "error_count=${BASH_REMATCH[3]}" 0 "Saving error_count"
+				rlRun "warning_count=${BASH_REMATCH[4]}" 0 "Saving warning_count"
+				rlRun "filtered_count=${BASH_REMATCH[6]:-?}" 0 "Saving filtered_count"
+				rlRun "badness_count=${BASH_REMATCH[7]:-?}" 0 "Saving badness_count"
+				break
+			fi
+		done < "$rlRun_LOG"
 	rlPhaseEnd
 
 	rlPhaseStartCleanup
@@ -38,3 +49,31 @@ rlJournalStart
 		rlRun "rm -r $tmp" 0 "Remove tmp directory"
 	rlPhaseEnd
 rlJournalEnd
+
+# Report the test results
+source $BEAKERLIB_DIR/TestResults
+if [[ ${TESTRESULT_RESULT_STRING,,} != pass ]]; then
+	# If test failed or triggerred warning within the test itself, use that outcome
+	result=${TESTRESULT_RESULT_STRING,,}
+else
+	# Otherwise take the highest result status of the rpmlint results
+	if (( error_count > 0 )); then
+		result=fail
+	elif (( warning_count > 0 )); then
+		result=warn
+	elif (( badness_count > 0 )); then
+		result=info
+	else
+		result=pass
+	fi
+fi
+# Write the actual results.yaml
+cat <<EOF >> "$TMT_TEST_DATA/results.yaml"
+- name: /
+  result: ${result}
+  note: ${error_count} errors, ${warning_count} warnings, ${filtered_count} filtered, ${badness_count} badness
+  log:
+    - ../output.txt
+    - ../journal.txt
+    - ../journal.xml
+EOF
