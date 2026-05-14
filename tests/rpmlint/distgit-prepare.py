37c6c8b4e045e4ef15a28f091a2edb8ec3183ede
Cristian Le <git@lecris.dev>
2026-05-14 14:19:56 +0200

    Move dist-git handling to its own function

--- tests/rpmlint/distgit-prepare.py
+++ tests/rpmlint/distgit-prepare.py
@@ -57,6 +57,46 @@ def get_config_from_ci_yaml(dist_git_path: Path) -> dict[str, Any] | None:
     return config
 
 
+def get_dist_git(koji_task_id: str, workdir: Path) -> Path:
+    result = subprocess.run(
+        [
+            "koji",
+            "taskinfo",
+            "-v",
+            koji_task_id,
+        ],
+        capture_output=True,
+        text=True,
+        check=True,
+    )
+    task_info = result.stdout
+    task_error = result.stderr
+    logger.info(f"Task info output:\n{task_info}\nTask error:\n{task_error}")
+    source_match_obj = re.search(r"Source:\s*(.*)", task_info)
+    if source_match_obj is None:
+        logger.error(
+            "Could not find 'Source:' in koji taskinfo output. Maybe a 500 error? Please retry."
+        )
+        sys.exit(1)
+    source = source_match_obj.group(1)
+    source_match = re.search(r"git\+(?P<url>.*)#(?P<ref>.*)", source)
+    repo_url = source_match.group("url")
+    repo_ref = source_match.group("ref")
+
+    # Clone the dist-git used in the build
+    dist_git_path = workdir / "dist-git"
+    subprocess.run(
+        ["git", "clone", repo_url, dist_git_path],
+        check=True,
+    )
+    subprocess.run(
+        ["git", "checkout", "-d", repo_ref],
+        cwd=dist_git_path,
+        check=True,
+    )
+    return dist_git_path
+
+
 def set_config_files(config: dict[str, Any], args: argparse.Namespace) -> None:
     if rc_content := config.get("rc"):
         rc_content: str
@@ -92,45 +132,7 @@ def main(args: argparse.Namespace) -> None:
     """
     Prepare for rpmlint from a dist-git
     """
-    args.env_file: Path
-
-    # Get the basic build information from koji
-    result = subprocess.run(
-        [
-            "koji",
-            "taskinfo",
-            "-v",
-            args.koji_task_id,
-        ],
-        capture_output=True,
-        text=True,
-        check=True,
-    )
-    task_info = result.stdout
-    task_error = result.stderr
-    logger.info(f"Task info output:\n{task_info}\nTask error:\n{task_error}")
-    source_match_obj = re.search(r"Source:\s*(.*)", task_info)
-    if source_match_obj is None:
-        logger.error(
-            "Could not find 'Source:' in koji taskinfo output. Maybe a 500 error? Please retry."
-        )
-        sys.exit(1)
-    source = source_match_obj.group(1)
-    source_match = re.search(r"git\+(?P<url>.*)#(?P<ref>.*)", source)
-    repo_url = source_match.group("url")
-    repo_ref = source_match.group("ref")
-
-    # Clone the dist-git used in the build
-    dist_git_path: Path = args.workdir / "dist-git"
-    subprocess.run(
-        ["git", "clone", repo_url, dist_git_path],
-        check=True,
-    )
-    subprocess.run(
-        ["git", "checkout", "-d", repo_ref],
-        cwd=dist_git_path,
-        check=True,
-    )
+    dist_git_path = get_dist_git(args.koji_task_id, args.workdir)
 
     if config := get_config_from_ci_yaml(dist_git_path):
         set_config_files(config, args)
